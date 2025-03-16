85c95c91c9a1a36e65184612a7d10f4d9b42db9b
Dima Sob <sobolevskii.dmit@gmail.com>
2025-03-16 23:48:54 +0100

    refactor: reinstall all dependencies

--- news-api/src/index.ts
+++ news-api/src/index.ts
@@ -1,3 +1,4 @@
 console.log("Hello Dima");
-console.log("Hello World");
-console.log("Hello World_2");
+console.log(
+  "I spent a very long and painful time trying to install the TypeScript package. It seems to be installed. The rest of the task is not yet done."
+);
--- news-api/webpack.config.js
+++ news-api/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   devtool: "eval-source-map",
-  mode: "development",
+  mode: "production",
   entry: "./src/index.ts",
   module: {
     rules: [

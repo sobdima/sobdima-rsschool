0294ce7649a09c7469e2fe28a45352880f0748ed
Dima <79633255+sobdima@users.noreply.github.com>
2025-03-25 00:23:55 +0100

    refactor: delete news-api directory for a new deployment

--- news-api/src/index.ts
+++
@@ -1,4 +0,0 @@
-console.log("Hello Dima");
-console.log(
-  "I spent a very long and painful time trying to install the TypeScript package. It seems to be installed. The rest of the task is not yet done."
-);
